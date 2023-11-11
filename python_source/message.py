59b19760e9b4f655f12b93423f49789b4797d6b4
Toby <49291693+tobywan-kenobi@users.noreply.github.com>
2023-11-11 11:27:36 -0500

    Implemented skeleton of mesage.py file for managing messages in the JSON database.

---
+++ python_source/message.py
@@ -0,0 +1,13 @@
+import json
+
+class Message():
+    def __init__(self, content, time):
+        self.content = content
+        self.time = time
+
+
+def load_messages(): # loads messages from json file and returns as list of messages
+    pass
+
+def add_message_to_json(message): # adds messages to json file storing the messages
+    pass
