da2d68247513afbaed051f2ca688bb5028c24b1b
Toby <49291693+tobywan-kenobi@users.noreply.github.com>
2023-11-11 16:17:58 -0500

    Implemented basic api functionality for viewing and sending messages via the /get_messages route and the /send_message route respectively.

--- python_source/app.py
+++ python_source/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+
+import message
 
 app = Flask(__name__)
 
 @app.route("/get_messages")
-def main_view():
-    return "Test"
+def messages_view():
+    return message.load_messages_to_json()
+
+
+@app.route("/send_message", methods=["POST"])
+def message_send():
+    message_content = request.form.get("message_content")
+    message_time = request.form.get("message_time")
+
+    message.add_message_to_json(message.Message(message_content, message_time)) 
+
+    return "200"
