396edf1a4071b5ab0180aed203a374a5fbd3b3e6
Arush Rastogi <46365165+ArRstgi@users.noreply.github.com>
2023-11-11 22:26:15 -0500

    Completed Sentiment Analyzer and Sentiment Manipulator in identification.py and updated requirements accordingly

--- python_source/identification.py
+++ python_source/identification.py
@@ -1,15 +1,26 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
 
-def sentimentAnalyzer(testToBeAnalyzed:str)->bool:
+def sentimentAnalyzer(textToBeAnalyzed:str)->bool:
     tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
     model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-    tokens = tokenizer.encode(testToBeAnalyzed, return_tensors='pt')
+    tokens = tokenizer.encode(textToBeAnalyzed, return_tensors='pt')
     result = model(tokens)
     outOfFiveStars = int(torch.argmax(result.logits))+1
     if outOfFiveStars > 3:
         return True
     return False
 
-if __name__ == '__main__':
-    print(sentimentAnalyzer('Arush'))
+def sentimentManipulator(textToBeManipulated:str)->str:
+    prompt = textToBeManipulated
+    listOfWords = prompt.split(" ")
+    print(listOfWords)
+    numberOfWords = len(listOfWords) * 5
+    print(numberOfWords)
+    generatedText = "bad"
+    epochs = 20
+    while (sentimentAnalyzer(generatedText)==False and epochs > 0):
+        generator = pipeline(task="text2text-generation", model="cffl/bart-base-styletransfer-subjective-to-neutral", max_length=numberOfWords)
+        generatedText = (generator(prompt)[0]['generated_text'])
+        epochs = epochs - 1
+    return generatedText
