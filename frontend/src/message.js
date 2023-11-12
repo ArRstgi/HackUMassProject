ea922f4ad754ef97456387cddbe96641acf20015
Toby <49291693+tobywan-kenobi@users.noreply.github.com>
2023-11-12 00:56:32 -0500

    Fixed formatting issues with message

--- frontend/src/message.js
+++ frontend/src/message.js
@@ -1,11 +1,21 @@
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import React, { useState } from 'react';
-import { Avatar, Card, Skeleton, Switch } from 'antd';
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import React, { useState } from "react";
+import { Avatar, Card, Skeleton, Switch } from "antd";
 const { Meta } = Card;
 
 function Message(props) {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '20vh' }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <div>
         <Card
           style={{
@@ -13,13 +23,11 @@ function Message(props) {
             marginTop: 16,
           }}
         >
-        <Meta
-          title={props.message_content}
-        />
+          {props.message_content}
         </Card>
       </div>
     </div>
   );
-};
+}
 
 export default Message;
